25dd6315b7477d3632c77db2dd175378755b3dcb
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-09-03 17:36:13 -0400

    Implement fibs_rec method

--- fibonacci.rb
+++ fibonacci.rb
@@ -13,8 +13,20 @@ def fibs(num)
   fib_sequence
 end
 
+def fibs_rec(num)
+  return [] if num.zero?
+  return [0] if num == 1
+  return [0, 1] if num == 2
+
+  sequence = fibs_rec(num - 1)
+  sequence << (sequence[-1] + sequence[-2])
+end
+
 print 'Please enter a number: '
 num = gets.to_i
 
 puts 'Using fibs method:'
 p fibs(num)
+
+puts "\nUsing fibs_rec method:"
+p fibs_rec(num)
